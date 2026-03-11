de0aedf146ffffb174cea9d7efc71595418fe04e
prosperReigns <prosperkinging7@gmail.com>
2026-03-11 09:34:31 +0100

    create build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# install dependencies
+pip install -r requirements.txt
+
+# run migrations
+python backend/config/manage.py migrate
+
+# collect static files
+python backend/config/manage.py collectstatic --noinput
